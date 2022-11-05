2a7e08977ee3b6a51036e35aa5f9345de89df019
abhi27033 <117388925+abhi27033@users.noreply.github.com>
2022-11-05 12:57:17 +0530

    Time: 27 ms (6.31%), Space: 13.9 MB (30.94%) - LeetHub

---
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int minCostClimbingStairs(vector<int>& cost) {
+        int n=cost.size();
+        vector<int>a(n+1);
+        for (int i=2;i<=n;i++){
+            int one=a[i-1]+cost[i-1];
+            int two=a[i-2]+cost[i-2];
+            a[i]=min(one,two);
+        }
+        return a[a.size()-1];
+    }
+};
