1daa48f7c00e27aecfc96a924998944ca77e9cd4
abhi27033 <117388925+abhi27033@users.noreply.github.com>
2022-11-08 18:22:27 +0530

    Time: 10 ms (20.25%), Space: 7.4 MB (5.40%) - LeetHub

--- 1544-make-the-string-great/1544-make-the-string-great.cpp
+++ 1544-make-the-string-great/1544-make-the-string-great.cpp
@@ -1,25 +1,18 @@
 class Solution {
 public:
-    string makeGood(string s) {
-        while(s!=good(s)){
-            s=good(s);
-            if(s=="")
-                break;
+    string makeGood(string s) {  
+        // If the string is emtpy, just return it.
+        if (s.empty()) 
+            return s;
+        
+        // If we find a pair in 's', remove this pair from 's'
+        // and solve the remaining string recursively.
+        for (int i = 0; i < s.size() - 1; ++i) {
+            if (abs(s[i] - s[i + 1]) == 32)
+                return  makeGood(s.substr(0, i) + s.substr(i + 2));
         }
-        return s;
-    }
-    string good(string s)
-    {
-        for(int i=0;i<s.length()-1;i++)
-        {
-           if((isupper(s[i])&&s[i+1]==tolower(s[i]))||(islower(s[i])&&s[i+1]==toupper(s[i])))    
-           {
-               s=s.substr(0,i)+s.substr(i+2);
-               if(s=="")
-                   return s;
-               i--;
-           }
-        }
+
+        // Base case, if we can't find a pair, just return 's'.
         return s;
     }
 };
