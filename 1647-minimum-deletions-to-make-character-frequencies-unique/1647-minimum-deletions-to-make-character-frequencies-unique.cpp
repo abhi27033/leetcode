fe38cbe63815ff6d0466c16c9524278f03a0f59b
abhi27033 <117388925+abhi27033@users.noreply.github.com>
2022-11-22 23:11:28 +0530

    Time: 326 ms (6.30%), Space: 18.2 MB (5.11%) - LeetHub

--- 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.cpp
+++ 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.cpp
@@ -9,34 +9,14 @@ public:
          int ar[v.size()],x=0;
         for(auto it:v)
         {
-           // cout<<it.first<<" "<<it.second<<endl;
             ar[x++]=it.second;
         }
        sort(ar,ar+v.size());
         //int n=v.size();
         int ans=0;
         stack<int> xx;
-        /*set<int> tp;
-        for(int i=0;i<x;i++)
-            tp.insert(ar[i]);*/
         int z=1;
-        /*for(auto it:tp)
-        {
-            //cout<<it<<endl;
-            while(it>z)
-            {
-                xx.push(z);
-                z++;
-            }
-            z++;
-        }*/
-        /*cout<<endl;
-        while(!(xx.empty()))
-        {
-            cout<<xx.top()<<" ";
-            xx.pop();
-        }
-        cout<<endl;*/
+        
         for(int i=0;i<x-1;i++){
            while(ar[i]>z)
            {
@@ -51,9 +31,6 @@ public:
                     ans+=(ar[i]-xx.top());
                     xx.pop();
                     }
-                //i++;
-             
-                
             }
             else 
                 z++;
