d389aac7ee4a030802dc5782a1d340b9220ef6e6
abhi27033 <117388925+abhi27033@users.noreply.github.com>
2022-11-22 21:53:01 +0530

    Time: 62 ms (93.49%), Space: 6 MB (91.59%) - LeetHub

---
+++ 0279-perfect-squares/0279-perfect-squares.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int numSquares(int n) {
+        int dp[n+1];
+        for(int i=0;i<=n;i++)
+            dp[i]=n;
+        dp[0]=0;
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=1;j<=i;j++)
+            {
+                int s=j*j;
+                if(i-s<0)
+                    break;
+                dp[i]=min(dp[i],1+dp[i-s]);
+            }
+        }
+        return dp[n];
+    }
+};
