d7ad321dc9d8e9c90df5293051f7c7d361283378
abhi27033 <117388925+abhi27033@users.noreply.github.com>
2022-11-25 23:56:06 +0530

    Time: 191 ms (66.26%), Space: 39.6 MB (88.97%) - LeetHub

--- 0907-sum-of-subarray-minimums/0907-sum-of-subarray-minimums.cpp
+++ 0907-sum-of-subarray-minimums/0907-sum-of-subarray-minimums.cpp
@@ -1,55 +1,30 @@
 class Solution {
 public:
-    vector<int> nextSmaller(vector<int> &a){
-        int n = a.size();
-        stack<pair<int,int>> s;
-        vector<int> ans(n,0);
-        for(int i = n-1 ; i >= 0 ; i--){
-            while(s.size()>0 and s.top().first >= a[i]){
-                s.pop();
+    int sumSubarrayMins(vector<int>& arr) {
+        int n=arr.size();
+        int dp[n];
+        stack<int> stk;
+        for(int i=0;i<n;i++)
+        {
+            while(!(stk.empty())&&arr[stk.top()]>=arr[i])
+                stk.pop();
+            if(stk.empty())
+            {
+                dp[i]=(i+1)*arr[i];
             }
-            if(s.size()==0){
-                ans[i] = n;
+            else
+            {
+                int j=stk.top();
+                dp[i]=dp[j]+(i-j)*arr[i];
             }
-            else{
-                ans[i] = s.top().second;
-            }
-            s.push({a[i],i});
+            stk.push(i);
         }
-        return ans;
-    }
-    vector<int> prevSmaller(vector<int> &a){
-        int n = a.size();
-        stack<pair<int,int>> s;
-        vector<int> ans(n,0);
-        for(int i = 0 ; i < n ; i++){
-            while(s.size()>0 and s.top().first > a[i]){
-                s.pop();
-            }
-            if(s.size()==0){
-                ans[i] = -1;
-            }
-            else{
-                ans[i] = s.top().second;
-            }
-            s.push({a[i],i});
+        int ans=0;
+        for(int i=0;i<n;i++)
+        {
+            ans+=(dp[i]);
+            ans%=1000000007;
         }
-        return ans;
-    }
-    long long mod_mul(long long a, long long b, int mod){
-        return (((a%mod)*(b%mod))+mod)%mod;
-    }
-    int sumSubarrayMins(vector<int>& a) {
-        vector<int> next = nextSmaller(a);
-        vector<int> prev = prevSmaller(a);
-        long long ans = 0; int mod = 1e9+7;
-        int n = a.size();
-        for(int i = 0 ; i < n; i ++){
-            int right = next[i]-i;
-            int left = i-prev[i];
-            ans += mod_mul(mod_mul(left,right,mod),a[i],mod);
-            ans%=mod;
-        }
-        return ans;
+            return ans%1000000007;
     }
 };
